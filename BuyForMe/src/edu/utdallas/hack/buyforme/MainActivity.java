383d76dd1b6c382a1b062da6b3f30894bdd9c0bb
upendrag <upendrag.gowda@gmail.com>
2015-02-28 22:00:38 -0600

    Checking SignUpActivity

--- BuyForMe/src/edu/utdallas/hack/buyforme/MainActivity.java
+++ BuyForMe/src/edu/utdallas/hack/buyforme/MainActivity.java
@@ -1,9 +1,13 @@
 package edu.utdallas.hack.buyforme;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 
 public class MainActivity extends Activity {
@@ -11,11 +15,26 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);        
+        setContentView(R.layout.activity_main);
+        
+        Button verifyPhoneButton = (Button) findViewById(R.id.verifyPhoneButton);
+        verifyPhoneButton.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View view) {
+				onVerifyPhoneClicked(view);
+			}
+		});
     }
 
 
-    @Override
+    protected void onVerifyPhoneClicked(View view) {
+		Intent intent = new Intent(this, SignUpActivity.class);
+		startActivity(intent);
+	}
+
+
+	@Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
---
+++ BuyForMe/src/edu/utdallas/hack/buyforme/SignUpActivity.java
@@ -0,0 +1,12 @@
+package edu.utdallas.hack.buyforme;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SignUpActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+}
